2b19751e15a756a5a146dd3b32edce005b689927
akash <akashbalachandar616@gmail.com>
2024-08-03 10:22:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+function App() {
+
+  return (
+    <div >
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
+  
